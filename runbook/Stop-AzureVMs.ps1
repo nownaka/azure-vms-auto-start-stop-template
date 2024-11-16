d35da37acdd11e441b2a793917e907df247bf148
nownaka <98019169+nownaka@users.noreply.github.com>
2024-11-16 19:25:46 +0900

    Update runbook files.

--- runbook/Stop-AzureVMs.ps1
+++ runbook/Stop-AzureVMs.ps1
@@ -1,3 +1,10 @@
+# ============================================================================
+# VMを停止するスクリプト
+# Created by nownaka.
+# ============================================================================
+
+Write-Output "Start!"
+
 ## 認証 ##
 # https://learn.microsoft.com/ja-jp/azure/automation/enable-managed-identity-for-automation#authenticate-access-with-system-assigned-managed-identity
 
@@ -9,3 +16,43 @@ $AzureContext = (Connect-AzAccount -Identity).context
 
 # Set and store context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+
+
+## VM 操作 ##
+# VM 一覧を取得
+Write-Output "Get VM List..."
+$vmList = Get-AzVM | Select-Object ResourceGroupName, Id, VmId, Name
+Write-Output $vmList
+
+# VM の電源ステータスを取得
+# https://learn.microsoft.com/ja-jp/azure/virtual-machines/windows/tutorial-manage-vm#vm-power-states
+# Starting	    |   起動中
+# Running       |   実行中
+# Stopping	    |   停止中
+# Stopped	    |   停止済(コスト発生)
+# Deallocating  |   割り当て解除中
+# Deallocated   |   割り当て解除済み(コスト発生なし)
+# -             |   電源状態は不明
+Write-Output "Get VM Power Status..."
+$vmPowerStatusList = @()
+foreach($vm in $vmList) {
+    $vmPowerStatusList += Get-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Status |
+    Select-Object ResourceGroupName, Name, @{ n = "Status"; e = { $_.Statuses[1].Code} }
+}
+Write-Output $vmPowerStatusList
+
+# VM を停止
+# https://learn.microsoft.com/ja-jp/powershell/module/az.compute/stop-azvm?view=azps-12.4.0
+Write-Output "Stop VMs..."
+$statusList = @("*Starting","*Running")
+foreach($vm in $vmPowerStatusList) {
+    foreach($status in $statusList) {
+        $flag = $vm.Status -like $status
+    }
+    if($flag) {
+        Write-Output "name: $($vm.Name)"
+        Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
+    }
+}
+
+Write-Output "Finish!"
